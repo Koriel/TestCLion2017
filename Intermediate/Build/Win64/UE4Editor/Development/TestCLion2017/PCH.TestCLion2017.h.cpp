4136a5daa4826f6ef015dd8b7924cdc27f03343d
Michael Spellman <mwspellman@gmail.com>
2017-02-08 13:21:44 -0800

    Intermediate project files without 'remote: error: File Intermediate/Build/Win64/UE4Editor/Development/TestCLion2017/TestCLion2017.h.pch'

---
+++ Intermediate/Build/Win64/UE4Editor/Development/TestCLion2017/PCH.TestCLion2017.h.cpp
@@ -0,0 +1,1 @@
+#include "C:\UnrealProjects\TestCLion2017\Source\TestCLion2017\TestCLion2017.h"
---
+++ Intermediate/Build/Win64/UE4Editor/Development/TestCLion2017/TestCLion2017.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_TESTCLION2017_H__
+#define __AUTO_TESTCLION2017_H__
+//Last Write: 2/8/2017 8:46:43 PM +00:00
+#include "C:\UnrealProjects\TestCLion2017\Source\TestCLion2017\TestCLion2017.h"
+#endif//__AUTO_TESTCLION2017_H__
